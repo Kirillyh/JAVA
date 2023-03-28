e7798b06efcd7d637c8e3c0f2df159f6736be393
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2023-03-28 13:55:23 +0300

    Добавили

---
+++ Main.java
@@ -0,0 +1,9 @@
+package seminar1;
+
+import seminar1.task.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Task1.run();
+    }
+}
---
+++ task/Task1.java
@@ -0,0 +1,12 @@
+package seminar1.task;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.err.println("world");
+    }
+
+    public static void run() {
+    }
+
+  
+}
