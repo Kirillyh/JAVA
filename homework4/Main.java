bed31cb7e19a250089279649f48c2702dcc04c15
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2023-04-08 11:49:54 +0300

    Добавили 1 задачу семинара 4

---
+++ homework4/Main.java
@@ -0,0 +1,11 @@
+package seminar1.homework4;
+
+import seminar1.homework4.tasks.*;
+
+public class Main {
+        public static void main(String[] args) {
+            System.out.print("\033[H\033[J");
+            task1.run();
+            
+        }
+}
---
+++ homework4/tasks/Main.java
@@ -0,0 +1,5 @@
+package seminar1.homework4.tasks;
+
+public class Main {
+    
+}
